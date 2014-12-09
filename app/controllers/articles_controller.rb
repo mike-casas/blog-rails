63d2ddd52ca95760f2015032c16ea15d9d981a11
mike-casas <neneriostb@gmail.com>
2014-12-08 19:07:15 -0500

    custom frontend index post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "nene", password: "1234", except: [:index, :show]
+
+
+
   def index
       @articles= Article.all
+      @article_order= Article.last_article
   end
 
   def new
@@ -18,6 +23,29 @@ class ArticlesController < ApplicationController
   def show
     @article=Article.find(params[:id])
   end
+
+  def edit
+    @article=Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(parametros)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+      @article= Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+  end
+
+
+
 private
   def parametros
     params.require(:article).permit(:title,:text)
